13dc612a717660e075b9ffb54c9acd4845de11e5
Abnergabrel <abnergssg@gmail.com>
2025-01-29 11:06:24 -0300

    Ex1

---
+++ Exercicios/lista2.js
@@ -0,0 +1,65 @@
+//ex1
+
+console.log("Exercicio")
+let a = 19
+let b = 16
+
+let maior =a > b
+console.log(maior)
+
+let menor = a <= b
+console.log(menor)
+
+
+let igual = a == b
+console.log(igual) 
+
+console.log("-------------------")
+console.log("Exercício 2")
+
+let MenorIdade = 15
+let MaiorIdade = 19
+
+let OpcionalVotar = MenorIdade >= 16
+let ObriadoVotar = MaiorIdade >= 18
+console.log(OpcionalVotar)
+console.log(ObriadoVotar)
+
+console.log("----------------------------")
+console.log("Exercicio 3")
+
+let idade = 90
+
+let MaiordeIdade = idade >= 18
+let idoso = idade >= 60
+console.log (MaiordeIdade)
+console.log (idoso)
+
+console.log("--------------------------")
+console.log("Exercicio 4")
+
+let usuario = "Abner"
+
+console.log(usuario == "admin")
+
+console.log("--------------------------")
+console.log("Exercicio 5")
+
+let numb1 = 7
+let numb2 = 11
+
+let diviTres = numb1 % 3
+let diviCinco = numb2 % 5
+console.log(diviTres)
+console.log(diviCinco)
+
+console.log("--------------------------")
+console.log("Exercicio 6")
+
+let saldoCont = 1000
+let SaldoSaq = 105
+
+let saldosuficiente = valorsaq < saldoCont
+let saldosuNficiente = saldoCont - valorSaq > 100
+console.log(saldosuficiente)
+console.log(saldoNsuficiente)

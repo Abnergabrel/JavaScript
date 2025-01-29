2d4439015e5fe2708e7342cac11091124414f6f4
Abnergabrel <abnergssg@gmail.com>
2025-01-29 09:56:58 -0300

    Conclusão

--- Exercicios/lista-1.js
+++ Exercicios/lista-1.js
@@ -1,6 +1,6 @@
 //Exercicio 1
 let a =1
-let b = 4
+let b = 7
 
 let soma = a + b
 console.log(soma)
@@ -17,10 +17,39 @@ console.log(soma)
 let resto = a + b
 console.log(soma)
 
+
 //Exercicio 2
 
 let altura = 10
 let largura = 5
 
 let area = altura * largura
+console.log (area)
 
+
+//Exercício 3
+
+let c = 5
+
+let dobro = c * 2
+console.log (dobro)
+
+let metade = c / 2
+console.log (metade)
+
+//Exercício 4
+
+let n1 = 7
+let n2 = 5
+let n3 = 8
+
+let media = (n1 + n2 + n3) / 3
+console.log (media)
+
+
+//Exercício 5
+
+let celsius = 30
+
+let fahrenheit = (celsius * 9/5) + 32
+console.log (fahrenheit)
---
+++ Operadores/relacionais.js
@@ -0,0 +1,34 @@
+/* > maior
+>= maior ou iguak
+< menor
+<= menor igual
+== igual
+!=diferente */
+
+let idade = 18
+let resultado = idade > 18 //MAIOR
+let resultado2 = idade >= 18 //MAIOR
+let resultado3 = idade < 18 //MENOR
+let resultado4 = idade <= 18 //MENOR OU IGUAL
+let resultado5 = idade == 18 //IGUAL
+let resultado6 = idade = 18 //DIFERENTE
+console.log(resultado)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
+console.log(resultado5)
+console.log(resultado6)
+
+console.log("-----------------------------------------------------")
+
+let dinheiro = 100
+let compra = 40
+let total = dinheiro - compra
+let situacao = total > 0
+console.log(situacao)
+console.log("-----------------------------------------------------")
+console.log("Outro exemplo")
+//saber se o numero é divisivel por outro
+let numero = 10 
+let disvisivel = 10 % 2
+console.log(disvisivel)

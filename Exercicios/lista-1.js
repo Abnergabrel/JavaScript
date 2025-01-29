87122abf7ccb8537da0008f9459436260df49a0f
Abnergabrel <abnergssg@gmail.com>
2025-01-29 08:19:02 -0300

    Initial commit

---
+++ Exercicios/lista-1.js
@@ -0,0 +1,26 @@
+//Exercicio 1
+let a =1
+let b = 4
+
+let soma = a + b
+console.log(soma)
+
+let sub = a + b
+console.log(soma)
+
+let mult = a + b
+console.log(soma)
+
+let div = a + b
+console.log(soma)
+
+let resto = a + b
+console.log(soma)
+
+//Exercicio 2
+
+let altura = 10
+let largura = 5
+
+let area = altura * largura
+
---
+++ Operadores/aritimeticos.js
@@ -0,0 +1,28 @@
+// console.log ("Olá, Mundo!")
+
+// para declarar variáveis
+let nome = "Abner" //vamos preferir esse
+var nomes = "Abner"
+
+//CTRL + ; -> COMENTARIOS
+console.log(nome)
+
+let idade = 18
+console.log("Minha idade é", idade, "anos")
+
+
+// OPERAÇÕES MATEMÁTICAS
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let resto = a % b
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
